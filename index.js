7c9b1a3dd37a45e4a53fb169e6e35c5ef68b25d1
Cibapatel <72306292+Cibapatel@users.noreply.github.com>
2023-03-07 23:58:14 +0530

    update

--- index.js
+++ index.js
@@ -17,3 +17,94 @@ function newQuote() {
 document.getElementById("quotedisplay").innerHTML = quote[randomNumber];
 
 }
+function validate(input){
+    //var mail = document.getElementById("text1").value;
+    var regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+    if(input.value.match(regx)){
+        return true; 
+    }
+
+    else{
+        alert("Please provide valid email and password")
+        document.form1.text1.focus();
+        return false;
+    }
+}
+//Booking table
+function Validateform() {
+    if(document.Myform.Name.value == ''){
+        alert("Please fill all the details");
+        document.Myform.Name.focus();
+        return false;
+    }
+    else if(document.Myform.Phone.value == ''){
+        alert("Please fill all the details");
+        document.Myform.Phone.focus();
+        return false;
+}
+else if(document.Myform.Date.value == ''){
+        alert("Please fill all the details");
+        document.Myform.Date.focus();
+        return false;
+}
+else if(document.Myform.Time.value == ''){
+        alert("Please fill all the details");
+        document.Myform.Time.focus();
+        return false;
+}
+else if(document.Myform.Number.value == ''){
+        alert("Please fill all the details");
+        document.Myform.Number.focus();
+        return false;
+}
+else{
+    alert("Thankyou! your form has been successsfully submitted")
+    return true;
+}
+
+}
+document.addEventListener('DOMContentLoaded', function(){
+    const list = document.querySelector('#cart-list ul');
+    const forms = document.forms;
+
+    //delete 
+
+    list.addEventListener('click',(e)=>{
+        if(e.target.className == 'delete'){
+            const li = e.target.parentElement;
+            li.parentNode.removeChild(li);
+        }
+        
+    })
+    list.addEventListener('click',(e)=>{
+        if(e.target.className == 'order'){
+            
+        }
+        
+    })
+    //adding 
+    const addForm = forms['add-cart']
+    addForm.addEventListener('submit',function(e){e.preventDefault();
+//creating elements
+const value = addForm.querySelector('input[type = "text"]').value;
+const li = document.createElement('li')
+const itemName = document.createElement('span');
+const deleteBtn = document.createElement('span');
+const orderBtn = document.createElement('span');
+//add text 
+
+itemName.textContent = value;
+deleteBtn.textContent = 'delete';
+orderBtn.textContent = 'Place order';
+//add classes
+itemName.classList.add('name');
+deleteBtn.classList.add('delete')
+orderBtn.classList.add('order')
+//apend to DOM
+li.appendChild(itemName);
+li.appendChild(deleteBtn);
+li.appendChild(orderBtn);
+list.appendChild(li)
+    })
+    
+})
